bde1c6186c1081a9cd172ceb2534317d6020d246
Frantisek Urban <fandau1@gmail.com>
2026-03-21 15:33:04 +0100

    update

--- include/pills_tracker.h
+++ include/pills_tracker.h
@@ -1,9 +1,20 @@
 #ifndef PILLS_TRACKER
 #define PILLS_TRACKER
 
+enum class DayMedicationState {
+    BEFORE_ALARM,
+    AT_ALARM,
+    AFTER_ALARM,
+    MEDICATION_COLLECTED
+};
+
 class PillsTracker {
     public:
-        void pill_taken();
+        void onPillTaken();
+
+    private:
+        DayMedicationState morningState;
+        DayMedicationState eveningState;
 };
 
 #endif // PILLS_TRACKER
